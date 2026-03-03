3466bbe8d5b9ef433751c1f7e937fa11330918ab
Deepak Gupta <deepakgupta@Deepaks-MacBook-Air.local>
2026-03-03 10:41:52 -0800

    Set canonical host to www and enforce apex redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://autonomybridge.com',
+  site: 'https://www.autonomybridge.com',
   integrations: [sitemap()],
 });
